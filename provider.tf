18a2aed0f452d4101ff617e9ceda27d704679d8b
Shoeb <49866549+shoebece@users.noreply.github.com>
2022-05-16 17:38:46 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,27 @@
 terraform {
   required_providers {
+    azuredevops = {
+      source = "microsoft/azuredevops"
+      version = "=0.1.7"
+    }
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "~>2.0"
+      version = "=2.75.0"
     }
   }
-
 }
+
+terraform {
+  backend "azurerm" {
+  }
+}
+
 provider "azurerm" {
   features {}
+}
+
+
+provider "azuredevops" {
+  org_service_url = "https://dev.azure.com/<org>"
+  personal_access_token = "<AZDO_PERSONAL_ACCESS_TOKEN>"
 }
